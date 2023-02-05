67aef8f56fb7097ffc7f4af5d33e95e53196c6ca
chaesuhyeon <ddd8177@gmail.com>
2023-02-05 17:50:02 +0900

    bottom navigatorBar tab 이동기능 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,89 @@
+import 'dart:js';
+
+import 'package:airbnb_app_clone/login.dart';
+import 'package:airbnb_app_clone/message.dart';
+import 'package:airbnb_app_clone/search.dart';
+import 'package:airbnb_app_clone/travel.dart';
+import 'package:airbnb_app_clone/wish_list.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    initialRoute: "/",
+    routes: {
+      "/" : (context) => MainPage(),
+      "/message" : (context) => Message(),
+      "/travel" : (context) => Travel(),
+      "/wish" : (context) => WishList(),
+      "login" : (context) => Login()
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: TabPage(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+// Bottom Navigation Bar
+// 동적으로 화면을 변화하므로 StatefulWdiget 사용
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+
+  int _selectedIndex = 0; // 처음에 나올 화면 지정
+
+  // 이동할 페이지
+  List _pages = [Search(),WishList(),Travel(),Message(), Login()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: Center(
+          child: _pages[_selectedIndex], // 페이지와 연결
+        ),
+
+        // BottomNavigationBar 위젯
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, // bottomNavigationBar item이 4개 이상일 경우
+
+          // 클릭 이벤트
+          onTap: _onItemTapped,
+
+          currentIndex: _selectedIndex, // 현재 선택된 index
+
+          // BottomNavigationBarItem 위젯
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "검색"),
+            BottomNavigationBarItem(icon: Icon(Icons.favorite_outline), label: "위시리스트"),
+            BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "여행"),
+            BottomNavigationBarItem(icon: Icon(Icons.message_outlined), label: "메시지함"),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: "로그인"),
+          ],
+          selectedItemColor: Colors.pink, // 선택된 요소 색
+          unselectedItemColor: Colors.grey, // 선택되지 않은 요소 색
+          selectedLabelStyle: TextStyle(fontSize: 10, fontWeight: FontWeight.bold , color:Colors.pink ), // 선택된 라벨 스타일
+          unselectedLabelStyle: TextStyle(fontSize: 10, fontWeight: FontWeight.bold , color: Colors.grey), // 선택되지 않은 라벨 스타일
+        )
+    );
+  }
+
+  void _onItemTapped(int index) {
+    // state 갱신
+    setState(() {
+      _selectedIndex = index; // index는 item 순서로 0, 1, 2로 구성
+    });
   }
 }
