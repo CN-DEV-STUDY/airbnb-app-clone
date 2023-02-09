0e570a51e090c2d44453f7c2a378006774edfe41
suhyeon chae <86937649+chaesuhyeon@users.noreply.github.com>
2023-02-09 23:50:35 +0900

    1.SignIn페이지 Stateless -> Stateful 로 변경 2. input 변화값 감지하여 변수에 할당

--- lib/signIn.dart
+++ lib/signIn.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+
+  // TextEditing 컨트롤러를 생성하여 필드에 할당
+  final textController = TextEditingController();
+
+  String email = "";
+  String pwd = "";
+
+  // _SignInState가 생성될 때 호출
+  @override
+  void initState() {
+    super.initState();
+    // textController
+    textController.addListener(_printLatestValue);
+  }
+
+  // _SignInState가 제거될 때 호출
+  @override
+  void dispose() {
+    // 텍스트에디팅컨트롤러를 제거하고, 등록된 리스너도 제거된다.
+    textController.dispose();
+    super.dispose();
+  }
+
+  // textController의 텍스트를 콘솔에 출력하는 메소드
+  void _printLatestValue() {
+    print("Second text field: ${textController.text}");
+  }
+
+  /**
+   * 텍스트필드의 값의 변화를 핸들링 하는 방법
+   * 1. 텍스트필드에 onChange 항목 구현
+   * 2. TextEditingController 인스턴스에 핸들링하는 함수를 리스너로 추가
+   */
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: PreferredSize( // appBar 높이 조정
         preferredSize: Size.fromHeight(30.0),
@@ -16,22 +55,37 @@ class SignIn extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Flexible(
-                flex:1,
-                child: Container(
-                    margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
-                    child:  Text('에어비앤비 로그인 / 회원가입' , style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold))),
-                ),
-            Flexible(
               flex:1,
-              child: Form(
-                child: ListView(
-                  children: [
-                    TextFormField(decoration: InputDecoration(labelText: 'ID'), textInputAction: TextInputAction.next,),
-                    TextFormField(decoration: InputDecoration(labelText: 'PWD'), textInputAction: TextInputAction.next,),
-                    Text('아이디와 비밀번호를 입력해주세요.' , style: TextStyle(fontSize: 10))
-                  ],
-                ),
-              )
+              child: Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                  child:  Text('에어비앤비 로그인 / 회원가입' , style: TextStyle(fontSize: 20 , fontWeight: FontWeight.bold))),
+            ),
+            Flexible(
+                flex:1,
+                child: Form(
+                  child: ListView(
+                    children: [
+                      TextFormField(
+                        onChanged: (text){
+                          setState(() {
+                            email = text;
+                            print("email : $email");
+                          });
+                        },
+                        decoration: InputDecoration(labelText: 'ID'),
+                        textInputAction: TextInputAction.next,),
+                      TextFormField(
+                        onChanged: (text){
+                          setState(() {
+                            pwd = text;
+                            print("pwd : $pwd");
+                          });
+                        },
+                      decoration: InputDecoration(labelText: 'PWD'), textInputAction: TextInputAction.next,),
+                      Text('아이디와 비밀번호를 입력해주세요.' , style: TextStyle(fontSize: 10))
+                    ],
+                  ),
+                )
             ),
             Flexible(
               flex:1,
@@ -54,3 +108,4 @@ class SignIn extends StatelessWidget {
     );
   }
 }
+
